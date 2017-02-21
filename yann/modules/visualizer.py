0b9c8c544d314d2a85367462ce18d269ad98d3ef
Jordan Rushing <rushing.jordan@gmail.com>
2017-02-21 15:34:23 -0600

    Modify imports and fix whitespace for quick-start compliance

--- yann/modules/visualizer.py
+++ yann/modules/visualizer.py
@@ -326,7 +326,8 @@ class visualizer (module):
             for id, activity in layer_activities.iteritems():
                 imgs = activity(index)
             if verbose >= 3:
-                print("... Visualizing Activities :: id = %s" % id)
+                print("... Visualizing Activities of id = %s" % id)
+            imgs = activity(index)
             if len(imgs.shape) == 2:
                 if not os.path.exists(loc + '/layer_' + id):
                     os.makedirs(loc + '/layer_' + id)
